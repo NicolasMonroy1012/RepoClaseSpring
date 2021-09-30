b2572a536a1fb36ea384315b75e96cb86a4b1d1c
NicolasMonroy1012 <91506577+NicolasMonroy1012@users.noreply.github.com>
2021-09-29 19:09:02 -0500

    Añadir src y mvm

---
+++ src/main/java/com/ciclo3/g22P/G22PApplication.java
@@ -0,0 +1,13 @@
+package com.ciclo3.g22P;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G22PApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G22PApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciclo3/g22P/G22PApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciclo3.g22P;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G22PApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
