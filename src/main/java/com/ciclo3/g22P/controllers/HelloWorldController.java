8febe7783b1bfcdff2e6a7f3fc2363b4ef676e1a
NicolasMonroy1012 <91506577+NicolasMonroy1012@users.noreply.github.com>
2021-09-29 19:17:23 -0500

    Create HelloWorldController.java

---
+++ src/main/java/com/ciclo3/g22P/controllers/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.ciclo3.g22P.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class HelloWorldController {
+    @GetMapping("")
+}
